b3f75114ae460e1d86cb4f98aa8a503f7ef0d551
Bander Saleh <bandersaleh93@gmail.com>
2023-03-23 00:33:00 -0700

    Initial commit

---
+++ DiceRoll.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    // Static class created to return data instantly and publicly
+    public static class DiceRoll
+    {
+        //The user can pass in a number, and return a value between 1 and that number
+        public static int RollTheDie(int diceSides) //Method if I wrote it like this instead "(this int diceSides)"
+        {
+            //Random rand = new Random(); // Initialized a new Instance of Random
+            //return rand.Next(1, diceSides + 1); //calculations to perform (mini version of a Method)
+            return diceSides;
+        }
+    }
+}
---
+++ UserAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MyClassLibrary
+{
+    public class UserAccount
+    {
+        // Fields
+        string _username;
+        string _password;
+
+        // Constructors
+        public UserAccount(string username, string password)
+        {
+            _username = username;
+            _password = password;
+        }
+
+        // Properties
+        public string Username { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
+
+
+        // Method Modifiers (Virtual, New, Override)
+        public override string? ToString()
+        {
+            return $"{_username} - {_password}";
+        }
+    }
+}
